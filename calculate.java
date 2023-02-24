e1146d10f3896085a76dc6c7d970ab8b01e3d2d4
Theeraphat-CH <124338336+Theeraphat-CH@users.noreply.github.com>
2023-02-24 13:32:06 +0700

    Create calculate.java

---
+++ calculate.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class calculate{
+    public static void main(String[] args ) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter NUmber : ");
+        int x = scan.nextInt();
+
+        System.out.println("Enter NUmber : ");
+        int y = scan.nextInt();
+
+        int result = x + y;
+        System.out.print(result);
+
+    }
+}
