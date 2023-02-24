10885d2951d3966ae31ed4210fc04360be966aeb
Theeraphat-CH <124338336+Theeraphat-CH@users.noreply.github.com>
2023-02-24 19:33:06 +0700

    Update calculate.java

---
+++ loopInteration.java
@@ -0,0 +1,11 @@
+public class loopInteration {
+    public static void main(String[] args) {
+        int n = 0;
+
+        while (true) {
+            if (n >= 5) break;
+            System.out.print(n);
+            n++;
+        }
+    }
+}
