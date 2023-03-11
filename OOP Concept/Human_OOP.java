243640c1d916e71b3888a9d4bfde0c55a3e8a691
Theeraphat-CH <124338336+Theeraphat-CH@users.noreply.github.com>
2023-03-11 18:08:33 +0700

    Update Human_OOP.java

--- OOP Concept/Human_OOP.java
+++ OOP Concept/Human_OOP.java
@@ -1,9 +1,9 @@
 public class Human_OOP {
     public static void main(String[] args) {
         Human human_data = new Human("Nate",18,80.0);
+        Human human_data2 = new Human("Natee",22,69.58);
 
-        System.out.printf("\nName : %s", Human.name);
-        System.out.printf("\nAge : %d", Human.age);
-        System.out.printf("\nWeight : %s", Human.weight);
+        human_data2.show();
+
     }
 }
