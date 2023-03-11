ef1bd3b81583c0ab8c375a17584a3939d2a32162
Theeraphat-CH <124338336+Theeraphat-CH@users.noreply.github.com>
2023-03-11 17:45:01 +0700

    OOP Concept (Get Data From Main)

---
+++ OOP Concept/Human.java
@@ -0,0 +1,7 @@
+public class Human {
+    Human(String name, int age, double weight) {
+        System.out.printf("\nName : %s", name);
+        System.out.printf("\nAge : %d", age);
+        System.out.printf("\nWeight : %s", weight);
+    }
+}
---
+++ OOP Concept/Human_OOP.java
@@ -0,0 +1,5 @@
+public class Human_OOP {
+    public static void main(String[] args) {
+        Human human_data = new Human("Nate",18,80.0);
+    }
+}
