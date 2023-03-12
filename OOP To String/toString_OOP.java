47c28c2ad02dee41f0e8601f792fb19cd0925f7b
Theeraphat-CH <124338336+Theeraphat-CH@users.noreply.github.com>
2023-03-12 19:22:57 +0700

    To String Method

---
+++ OOP To String/toString_OOP.java
@@ -0,0 +1,9 @@
+public class toString_OOP {
+    String hello = "hello";
+    String world = "world";
+    int number = 10;
+
+    public String toString() {
+        return hello +"\n"+ world +"\n"+ number;
+    }
+}
---
+++ OOP To String/toString_main.java
@@ -0,0 +1,22 @@
+public class toString_main {
+    public static void main(String[] args) {
+        toString_OOP toString = new toString_OOP();
+
+        /*
+            System.out.println(toSTring.hello);
+            System.out.println(toSTring.world);
+            System.out.println(toSTring.number);
+         */
+
+        /*
+            System.out.println(toString);
+
+            Output => toString_OOP@3fee733d //this is the address of the toString object in memory
+        */
+
+        System.out.println(toString.toString());
+
+        System.out.println(toString);
+
+    }
+}
