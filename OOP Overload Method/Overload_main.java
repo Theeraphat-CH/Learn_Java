f0201daddfd3856654b9a7ca3cbf6797ae59e77e
Theeraphat-CH <124338336+Theeraphat-CH@users.noreply.github.com>
2023-03-13 23:13:29 +0700

    OOP Overload

---
+++ OOP Overload Method/Overload_main.java
@@ -0,0 +1,14 @@
+public class Overload_main {
+    public static void main(String[] args) {
+        Overload_oop overOOP  = new Overload_oop();
+
+        int x = 5;
+        int y = 5;
+        int z = 5;
+        int w = 5;
+
+        int result = overOOP.result(x, y, z, w);
+
+        System.out.println((result));
+    }
+}
---
+++ OOP Overload Method/Overload_oop.java
@@ -0,0 +1,14 @@
+public class Overload_oop {
+    int result(int x, int y) {
+        return x + y;
+    }
+
+    int result(int x, int y, int z) {
+        return x + y + z;
+    }
+
+    int result(int x, int y, int z, int w) {
+        return x + y + z + w;
+    }
+    
+}
