fdf329b17faaa000cb9141a7b5c4f9100e86204d
Theeraphat-CH <124338336+Theeraphat-CH@users.noreply.github.com>
2023-03-14 16:23:51 +0700

    Update test_oop_main.java

--- OOP Array Of Object/test_oop_main.java
+++ OOP Array Of Object/test_oop_main.java
@@ -1,14 +1,17 @@
 public class test_oop_main {
     public static void main(String[] args) {
 
-        test_oop[] refrigerator = new test_oop[3];
+        //test_oop[] refrigerator = new test_oop[3];
 
         test_oop food1 = new test_oop("Hotdog");
         test_oop food2 = new test_oop("Hamburgers");
         test_oop food3 = new test_oop("Pad Thai");
-        refrigerator[0] = food1;
-        refrigerator[1] = food2;
-        refrigerator[2] = food3;
+
+        test_oop[] refrigerator = {food1,food2,food3};
+
+        //refrigerator[0] = food1;
+        //refrigerator[1] = food2;
+        //refrigerator[2] = food3;
 
         System.out.println(refrigerator[0].name);
         System.out.println(refrigerator[1].name);
